029274424c3473c81f1da867c832a4e0d06bfb45
ksk-6021 <sgkg2215462@gmail.com>
2021-06-29 19:06:08 +0900

    messagesコントローラー・ルーティングの設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  get 'messages/index'
   devise_for :users
   root to: 'posts#index'
   resources :posts, only: [:index, :new, :create, :show, :edit, :update, :destroy]
   resources :users, only: [:show, :edit, :update]
+  resources :messages, only: [:index]
 end
