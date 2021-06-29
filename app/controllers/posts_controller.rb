31c2c757eecb02d77300d008bd704cb68ee3ae22
ksk-6021 <sgkg2215462@gmail.com>
2021-06-29 11:48:14 +0900

    新規投稿機能の実装(モデル・コントローラー・ビューの設定）

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,49 @@
 class PostsController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
+  before_action :post_info, only: [:show, :edit, :update, :destroy]
+
   def index
     @posts = Post.all.order(created_at: :desc)
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    if @post.update(post_params)
+      redirect_to post_path
+    else
+      render :edit
+    end
+  end
+  
+  def destroy
+    @post.destroy
+    redirect_to root_path
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:trip_date, :place, :title, :content).merge(user_id: current_user.id)
+  end
+
+  def post_info
+    @post = Post.find(params[:id])
   end
 end
