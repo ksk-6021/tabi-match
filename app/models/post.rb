0145cf03126e57b01afaa3460ccdaa2cbdb0f00e
ksk-6021 <sgkg2215462@gmail.com>
2021-06-29 11:48:14 +0900

    新規投稿機能の実装(モデル・コントローラー・ビューの設定）

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,11 @@
 class Post < ApplicationRecord
   belongs_to :user
+  
+  with_options presence: true do
+    validates :trip_date
+    validates :place
+    validates :title
+    validates :content
+  end
+  
 end
