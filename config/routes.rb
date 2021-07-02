2e2f79c466eb58f878393860281fece682246a8b
ksk-6021 <sgkg2215462@gmail.com>
2021-07-02 13:47:03 +0900

    ルーム詳細機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
-  get 'messages/index'
   devise_for :users
   root to: 'posts#index'
   resources :posts, only: [:index, :new, :create, :show, :edit, :update, :destroy]
   resources :users, only: [:show, :edit, :update]
   resources :messages, only: [:index]
-  resources :rooms, only: [:new, :create]
+  resources :rooms, only: [:new, :create, :show]
 end
