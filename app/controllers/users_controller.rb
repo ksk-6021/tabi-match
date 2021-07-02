4d1691360eadc7ba499af81af5d6c8669121adea
ksk-6021 <sgkg2215462@gmail.com>
2021-07-02 23:41:35 +0900

    コード修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
-  before_action :authenticate_user!, only: [:edit, :update]
+  before_action :authenticate_user!, only: [:show, :edit, :update]
+  
   def show
     @user = User.find(params[:id])
     posts = Post.all.order(created_at: :desc)
