d2cff5ce41fcb0788085a7f3e079da1829cfcac4
ksk-6021 <sgkg2215462@gmail.com>
2021-07-03 00:30:46 +0900

    ユーザー制限の実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!, only: [:show, :edit, :update]
-  
+  before_action :user_verification,  only: [:edit, :update]
+
   def show
     @user = User.find(params[:id])
     posts = Post.all.order(created_at: :desc)
@@ -23,5 +24,9 @@ class UsersController < ApplicationController
 
   def user_params
     params.require(:user).permit(:name, :email, :introduction, :avatar)
+  end
+
+  def user_verification
+    redirect_to action: :index unless @post.user == current_user
   end
 end
