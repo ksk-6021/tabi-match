396472ee9e81c77f5c3e4c921acbf99f9b9609d5
ksk-6021 <sgkg2215462@gmail.com>
2021-07-02 13:48:40 +0900

    ルーム機能実装に伴うユーザー詳細画面の変更

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,7 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     posts = Post.all.order(created_at: :desc)
-    #@rooms = Room.all
+    @rooms = @user.rooms
     #@messages = Message.all
   end
 
