3c09d9b8a96156e9b4d6bf21ef9d31e0849affe5
ksk-6021 <sgkg2215462@gmail.com>
2021-07-02 13:59:32 +0900

    Messageモデルの作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   has_many :posts
+  has_many :messages
   has_one_attached :avatar
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
